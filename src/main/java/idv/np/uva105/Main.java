99cb5c004bc040248f94973ed3400776458ab5b9
nightpig <weilun0329@gmail.com>
2013-04-26 20:24:24 +0800

    commit uva105

---
+++ src/main/java/idv/np/uva105/Main.java
@@ -0,0 +1,43 @@
+package idv.np.uva105;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        List<int[]> bds = new ArrayList<int[]>();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int[] bd = new int[3];
+            bd[0] = sc.nextInt();
+            bd[1] = sc.nextInt();
+            bd[2] = sc.nextInt();
+            bds.add(bd);
+        }
+        sc.close();
+        int maxR = 0;
+        int[] data = new int[10000];
+        Arrays.fill(data, 0);
+        for (int i = 0, size = bds.size(); i < size; i++) {
+            int[] bd = bds.get(i);
+            for (int j = bd[0]; j < bd[2]; j++) {
+                data[j] = (bd[1] > data[j]) ? bd[1] : data[j];
+            }
+            maxR = (bd[2] > maxR) ? bd[2] : maxR;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= maxR; i++) {
+            if (data[i - 1] != data[i]) {
+                builder.append(i).append(" ").append(data[i]).append(" ");
+            }
+        }
+        System.out.println(builder.toString().trim());
+    }
+
+}
